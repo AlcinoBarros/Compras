0ab5d446550c5991cce2473e2970d71df5119a27
Alcino Jose <steveneizy1294@gmail.com>
2024-11-03 10:25:05 -0300

    Refatoração das telas do app

--- app/src/main/java/com/alcinojose/compras/MainActivity.kt
+++ app/src/main/java/com/alcinojose/compras/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.alcinojose.compras
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.alcinojose.compras.Model.AdicionarItemFragment
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tarefasRecyclerview: RecyclerView;
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,8 +26,21 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
-
         }
+
+        supportActionBar?.hide();
+
+        tarefasRecyclerview = findViewById(R.id.tarefasRecyclerView)
+        tarefasRecyclerview.setLayoutManager( LinearLayoutManager(this));
+
+        val fab: FloatingActionButton = findViewById(R.id.addTarefa)
+
+        fab.setOnClickListener{
+            val intent = Intent(this, AdicionarItemFragment::class.java)
+            startActivity(intent)
+        }
+
     }
+
+
 }

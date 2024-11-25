4928636ba8bf5bdc61acbafc80d8e2afef1c9c6e
Alcino Jose <steveneizy1294@gmail.com>
2024-11-25 13:38:16 -0300

    Adicionado o card do item.

--- app/src/main/java/com/alcinojose/gerenciadordetarefas/NovaTarefa.kt
+++ app/src/main/java/com/alcinojose/gerenciadordetarefas/NovaTarefa.kt
@@ -3,17 +3,25 @@ package com.alcinojose.gerenciadordetarefas
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class NovaTarefa () : BottomSheetDialogFragment() {
+class NovaTarefa : AppCompatActivity(){
 
-    fun onCreate(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+//    fun onCreate(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//        setContentView(R.layout.nova_tarefa)
+//    // Substitua pelo layout da sua Activity
+//        // ... (resto do código da sua Activity)
+//
+//    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.nova_tarefa)
 
     }
-
-
 }
--- app/src/main/java/com/alcinojose/gerenciadordetarefas/TarefaDatabase.kt
+++ app/src/main/java/com/alcinojose/gerenciadordetarefas/TarefaDatabase.kt
@@ -1,4 +1,47 @@
 package com.alcinojose.gerenciadordetarefas
 
-class TarefaDatabase {
+import android.content.Context
+import androidx.room.*
+
+
+@Database(entities = [TaskItem::class], version = 1, exportSchema = false)
+class TarefaDatabase : RoomDatabase() {
+
+    abstract fun taskItemDao(): TaskItemDao
+
+    companion object
+    {
+        @Volatile
+        private var INSTANCE: TaskItemDatabase? = null
+
+        fun getDatabase(context: Context): TaskItemDatabase
+        {
+            return INSTANCE ?: synchronized(this)
+            {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    TaskItemDatabase::class.java,
+                    "task_item_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
+
+@Dao
+interface TaskItemDao
+{
+    @Query("SELECT * FROM task_item_table ORDER BY id ASC")
+    fun allTaskItems(): Flow<List<TaskItem>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTaskItem(taskItem: TaskItem)
+
+    @Update
+    suspend fun updateTaskItem(taskItem: TaskItem)
+
+    @Delete
+    suspend fun deleteTaskItem(taskItem: TaskItem)
 }
